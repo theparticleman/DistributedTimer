9a39171482abedab52d585e726c337645cd86461
Jon Turner <theparticleman@gmail.com>
2020-04-03 14:31:32 -0600

    Removed unused usings

--- TimerService.cs
+++ TimerService.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Threading;
 using System.Threading.Tasks;
-using DistributedTimer.Hubs;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
